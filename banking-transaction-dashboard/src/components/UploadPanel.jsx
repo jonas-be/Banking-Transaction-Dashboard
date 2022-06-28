015005fcbd44541b0da1c559e2a2716b0307ba1e
Jonas Bender <jbender@united-internet.de>
2022-06-28 10:55:54 +0200

    Worked on Upload overlay

---
+++ banking-transaction-dashboard/src/components/UploadPanel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function UploadPanel() {
+  return (
+  <div></div>
+  // return (
+  //   <div className='fixed w-screen p-4 flex justify-center z-10 bg-[rgb(1,1,1,.25)]'>
+  //     <div className='w-3/4-screen h-3/4-screen rounded-3xl bg-white shadow-xl '>
+
+  //     </div>
+  //   </div>
+  )
+}
+
+export default UploadPanel
